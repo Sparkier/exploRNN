a1dfd6beccb8e5534ff408154e2982755bbb5ffc
Raphael <raphael.stoerk@uni-ulm.de>
2019-11-03 22:43:05 +0100

    refactored the lstm model sketch

--- src/components/p5Wrappers/sketches/model/lstm.js
+++ src/components/p5Wrappers/sketches/model/lstm.js
@@ -10,6 +10,7 @@ export class LSTM {
    * @param {object} s the p5 sketch
    */
   constructor(s) {
+    // defining general global values
     this.s = s;
     this.items = [];
     this.connections = [];
@@ -24,6 +25,7 @@ export class LSTM {
     s.half_orange = s.color(200, 150, 80);
     s.green = s.color(50, 175, 80);
 
+    // creating the cell components of the lstm cell
     this.items.push(
         this.receive =
           new Item('rec', 'Layer Input', left + horBuf, top + verBuf, 2, s)
@@ -35,7 +37,7 @@ export class LSTM {
     this.items.push(
         this.save =
           new Item('sav', 'Cell State Update',
-            left + 3*horBuf, top + verBuf, 2, s)
+              left + 3*horBuf, top + verBuf, 2, s)
     );
     this.items.push(
         this.forget =
@@ -82,86 +84,88 @@ export class LSTM {
           new Item('glt', '', left + 7 * horBuf, top + verBuf, 1, s)
     );
 
+    // setting uo the connections between the lstm cell items
     this.connections.push(
-        this.ghostInput =
+          this.ghostInput =
+            new Connection([
+              {x: this.ghostFirst.x, y: this.ghostFirst.y},
+              {x: this.first.x, y: this.first.y}], [this.first], s));
+    this.connections.push(
+        this.mainInput =
           new Connection([
-            {x: this.ghostFirst.x, y: this.ghostFirst.y},
-            {x: this.first.x, y: this.first.y}], [this.first], s));
+            {x: this.first.x, y: this.first.y},
+            {x: this.receive.x, y: this.receive.y}], [this.receive], s));
     this.connections.push(
-      this.mainInput =
-        new Connection([
-          {x: this.first.x, y: this.first.y},
-          {x: this.receive.x, y: this.receive.y}], [this.receive], s));
+        this.bus =
+          new Connection([
+            {x: this.receive.x, y: this.receive.y},
+            {x: this.receive.x, y: this.crossOutput.y},
+            {x: this.crossOutput.x, y: this.crossOutput.y}], [], s));
     this.connections.push(
-      this.bus =
-        new Connection([
-          {x: this.receive.x, y: this.receive.y},
-          {x: this.receive.x, y: this.crossOutput.y},
-          {x: this.crossOutput.x, y: this.crossOutput.y}], [], s));
+        this.toInput =
+          new Connection([
+            {x: this.crossInput.x, y: this.crossInput.y},
+            {x: this.add.x, y: this.add.y}], [this.add], s));
     this.connections.push(
-      this.toInput =
-        new Connection([
-          {x: this.crossInput.x, y: this.crossInput.y},
-          {x: this.add.x, y: this.add.y}], [this.add], s));
+        this.toForget =
+          new Connection([
+            {x: this.crossForget.x, y: this.crossForget.y},
+            {x: this.forget.x, y: this.forget.y}], [this.forget], s));
     this.connections.push(
-      this.toForget =
-        new Connection([
-          {x: this.crossForget.x, y: this.crossForget.y},
-          {x: this.forget.x, y: this.forget.y}], [this.forget], s));
+        this.toOutput =
+          new Connection([
+            {x: this.crossOutput.x, y: this.crossOutput.y},
+            {x: this.output.x, y: this.output.y}], [this.output], s));
     this.connections.push(
-      this.toOutput =
-        new Connection([
-          {x: this.crossOutput.x, y: this.crossOutput.y},
-          {x: this.output.x, y: this.output.y}], [this.output], s));
+        this.addToSave =
+          new Connection([
+            {x: this.add.x, y: this.add.y},
+            {x: this.save.x, y: this.save.y}], [this.save], s));
     this.connections.push(
-      this.addToSave =
-        new Connection([
-          {x: this.add.x, y: this.add.y},
-          {x: this.save.x, y: this.save.y}], [this.save], s));
+        this.forgetToSave =
+          new Connection([
+            {x: this.forget.x, y: this.forget.y},
+            {x: this.save.x, y: this.save.y}], [this.save], s));
     this.connections.push(
-      this.forgetToSave =
-        new Connection([
-          {x: this.forget.x, y: this.forget.y},
-          {x: this.save.x, y: this.save.y}], [this.save], s));
+        this.saveToCell =
+          new Connection([
+            {x: this.save.x, y: this.save.y},
+            {x: this.cell.x, y: this.cell.y}], [this.cell], s));
     this.connections.push(
-      this.saveToCell =
-        new Connection([
-          {x: this.save.x, y: this.save.y},
-          {x: this.cell.x, y: this.cell.y}], [this.cell], s));
+        this.cellOut =
+          new Connection([
+            {x: this.cell.x, y: this.cell.y},
+            {x: this.crossCell.x, y: this.cell.y},
+            {x: this.crossCell.x, y: this.crossCell.y}], [], s));
     this.connections.push(
-      this.cellOut =
-        new Connection([
-          {x: this.cell.x, y: this.cell.y},
-          {x: this.crossCell.x, y: this.cell.y},
-          {x: this.crossCell.x, y: this.crossCell.y}], [], s));
+        this.cellToForget =
+          new Connection([
+            {x: this.crossCell.x, y: this.crossCell.y},
+            {x: this.forget.x, y: this.forget.y}], [this.forget], s));
     this.connections.push(
-      this.cellToForget =
-        new Connection([
-          {x: this.crossCell.x, y: this.crossCell.y},
-          {x: this.forget.x, y: this.forget.y}], [this.forget], s));
+        this.cellToOutput =
+          new Connection([
+            {x: this.crossCell.x, y: this.crossCell.y},
+            {x: this.output.x, y: this.output.y}], [this.output], s));
     this.connections.push(
-      this.cellToOutput =
-        new Connection([
-          {x: this.crossCell.x, y: this.crossCell.y},
-          {x: this.output.x, y: this.output.y}], [this.output], s));
+        this.recurrent =
+          new Connection([
+            {x: this.output.x, y: this.output.y},
+            {x: this.output.x, y: top + 2.5 * verBuf},
+            {x: this.receive.x, y: top + 2.5 * verBuf},
+            {x: this.receive.x, y: this.receive.y}], [this.receive], s));
     this.connections.push(
-      this.recurrent =
-        new Connection([
-          {x: this.output.x, y: this.output.y},
-          {x: this.output.x, y: top + 2.5 * verBuf},
-          {x: this.receive.x, y: top + 2.5 * verBuf},
-          {x: this.receive.x, y: this.receive.y}], [this.receive], s));
+        this.mainOut =
+          new Connection([
+            {x: this.output.x, y: this.output.y},
+            {x: this.last.x, y: this.last.y}], [this.last], s));
     this.connections.push(
-      this.mainOut =
-        new Connection([
-          {x: this.output.x, y: this.output.y},
-          {x: this.last.x, y: this.last.y}], [this.last], s));
-    this.connections.push(
-      this.ghostOutput =
-        new Connection([
-          {x: this.last.x, y: this.last.y},
-          {x: this.ghostLast.x, y: this.ghostLast.y}], [this.ghostLast], s));
+        this.ghostOutput =
+          new Connection([
+            {x: this.last.x, y: this.last.y},
+            {x: this.ghostLast.x, y: this.ghostLast.y}], [this.ghostLast], s));
 
+    // defining which items have which outgoing connections
     this.ghostFirst.connections.push(this.ghostInput);
     this.first.connections.push(this.mainInput);
     this.receive.connections.push(this.bus);
@@ -183,30 +187,34 @@ export class LSTM {
     this.last.connections.push(this.ghostOutput);
     this.ghostLast.connections.push(this.ghostFirst);
 
+    // activating some items to start of the animation sequence
     this.ghostFirst.addActiveInput();
     this.recurrent.addActiveInput();
     this.forget.addActiveInput();
   }
 
+  /**
+   * the drawing function of the lstm model that gets called every frame
+   * the lstm cell should be visible on the p5 sketch canvas
+   */
   draw() {
     const s = this.s;
     s.rectMode(s.CENTER);
     s.fill(0, 100);
     s.noStroke();
-    s.rect(s.width/2+20, s.height/2+20, s.ctrWidth * s.ctrRatio, s.height * s.ctrRatio);
+    s.rect(s.width/2+20, s.height/2+20, s.ctrWidth * s.ctrRatio,
+        s.height * s.ctrRatio);
     s.fill(255);
     s.stroke(100);
     s.strokeWeight(15);
-    s.rect(s.width/2, s.height/2, s.ctrWidth * s.ctrRatio, s.height * s.ctrRatio);
-
+    s.rect(s.width/2, s.height/2, s.ctrWidth * s.ctrRatio,
+        s.height * s.ctrRatio);
     for (const c of this.connections) {
       c.draw();
     }
-
     for (const i of this.items) {
       i.draw();
     }
-
     if (s.clickedItem) {
       this.anim++;
       if (this.anim >= this.animMax) {
@@ -249,6 +257,10 @@ export class LSTM {
     }
   }
 
+  /**
+   * This functions sends an update trigger to all items and connections
+   * to check if one of them needs to forward their activation
+   */
   update() {
     for (const c of this.connections) {
       c.sendActivations();
@@ -264,12 +276,26 @@ export class LSTM {
     }
   }
 
+  /**
+   * A function that gets called if the user moves the mouse over the canvas
+   * and sends the trigger to all interactable items on the screen
+   * 
+   * @param {number} x the x position of the mouse cursor
+   * @param {number} y the y position of the mouse cursor
+   */
   mouseMoved(x, y) {
     for (const i of this.items) {
       i.mouseMoved(x, y);
     }
   }
 
+  /**
+   * This function gets called if the user has clicked on the screen, it
+   * then looks if the user has clicked on an interactable item and if so
+   * activates the according following functions
+   *
+   * @return {boolean} true, if the user has clicked on an item
+   */
   checkClick() {
     let ret = false;
     const s = this.s;
@@ -287,7 +313,18 @@ export class LSTM {
   }
 }
 
+/**
+ * This class represents a connection between two or more items
+ * in the lstm cell model
+ */
 class Connection {
+  /**
+   * The constructor function of the connection
+   *
+   * @param {array} verts the vertices of the connection line
+   * @param {Item} goesTo the item this connection connects to
+   * @param {object} s the p5 sketch
+   */
   constructor(verts, goesTo, s) {
     this.verts = verts;
     this.next = goesTo;
@@ -297,6 +334,10 @@ class Connection {
     this.activeInputs = 0;
   }
 
+  /**
+   * the drawing function of connection class, responsible for drawing
+   * the connection line according to the set values
+   */
   draw() {
     const s = this.s;
     s.noFill();
@@ -322,10 +363,19 @@ class Connection {
     this.s.fill(0);
   }
 
+  /**
+   * Increases the amount of active inputs, since connections only can
+   * have one active input this function is equal to setting the connection
+   * to active
+   */
   addActiveInput() {
     this.activeInputs += 1;
   }
 
+  /**
+   * If this current connection is active it will activate all following
+   * items, meaning all items this connection connects to
+   */
   sendActivations() {
     if (this.active && this.next && this.next.length > 0) {
       for (const n of this.next) {
@@ -334,6 +384,12 @@ class Connection {
     }
   }
 
+  /**
+   * this function is responsible for always keeping the activation status
+   * of this connection up to date, getting called at the end of each updating
+   * cycle to make sure no connection stays active/inactive when it
+   * shouldn't be
+   */
   updateActivation() {
     if (this.active) {
       this.active = false;
@@ -346,7 +402,20 @@ class Connection {
   }
 }
 
+/**
+ * This class represents all the items in a lstm cell model
+ */
 class Item {
+  /**
+   * The constructor function of the item class
+   *
+   * @param {string} type the item type represented as a string
+   * @param {string} name the name to be displayed when hovering over this item
+   * @param {number} x the absolute x position of this item
+   * @param {number} y the absolute y position of this item
+   * @param {number} ingoing the amount of ingoing connections
+   * @param {object} s the p5 sketch
+   */
   constructor(type, name, x, y, ingoing, s) {
     this.type = type;
     this.name = name;
@@ -377,6 +446,10 @@ class Item {
     }
   }
 
+  /**
+   * This function is responsible for drawing the item on the 
+   * drawing canvas
+   */
   draw() {
     const s = this.s;
     let size = this.r;
@@ -385,13 +458,6 @@ class Item {
       size = this.r * 1.2;
       imgSize = 0.6 * this.r * 1.2;
     }
-    this.s.fill(0, 70);
-    this.s.noStroke();
-    if (this.type === 'cel') {
-      // this.s.rect(this.x + size * 0.1, this.y + size * 0.1, size, size);
-    } else {
-      // this.s.ellipse(this.x + size * 0.1, this.y + size * 0.1, size);
-    }
     this.s.fill(54);
     this.s.noStroke();
     if (this.active) {
@@ -399,7 +465,8 @@ class Item {
     } else if (this.currentActivatedConnecions !== 0) {
       s.fill(s.half_orange);
     }
-    if (this.hover && !this.clicked && !(this.type === 'fst' || this.type === 'lst' || this.type === 'crs')) {
+    if (this.hover && !this.clicked &&
+        !(this.type === 'fst' || this.type === 'lst' || this.type === 'crs')) {
       s.fill(s.green);
       s.cursor(s.HAND);
     }
@@ -429,7 +496,8 @@ class Item {
         break;
       default:
     }
-    if (this.hover && !this.clicked && !(this.type === 'fst' || this.type === 'lst' || this.type === 'crs')) {
+    if (this.hover && !this.clicked &&
+        !(this.type === 'fst' || this.type === 'lst' || this.type === 'crs')) {
       s.textAlign(s.CENTER, s.CENTER);
       s.fill(0, 150);
       s.rect(s.mouseX, s.mouseY+40, 100, 30);
@@ -438,10 +506,19 @@ class Item {
     }
   }
 
+  /**
+   * this function increases the amount of active inputs by one, if the
+   * maximum amount of input activtaions is reached this item will fire
+   * an activation trigger in the next update cycle
+   */
   addActiveInput() {
     this.currentActivatedConnecions += 1;
   }
 
+  /**
+   * This funtion will send an activation call to all outgoing connections
+   * if the item has enough activation inputs
+   */
   sendActivations() {
     if (this.active && this.connections && this.connections.length > 0) {
       if (this.type === 'glt') {
@@ -459,6 +536,10 @@ class Item {
     }
   }
 
+  /**
+   * This function makes sure that the item is always correctly labeled
+   * active or inactive and gets called at the end of each update cycle
+   */
   updateActivation() {
     if (this.active) {
       this.active = false;
@@ -470,6 +551,13 @@ class Item {
     }
   }
 
+  /**
+   * This function is called if the user has moved the mouse across the
+   * canvas and checks if this item is being hovered over
+   *
+   * @param {number} x the x position of the mouse cursor
+   * @param {number} y the y position of the mouse cursor
+   */
   mouseMoved(x, y) {
     if (this.s.dist(x, y, this.x, this.y) < this.r/2) {
       this.hover = true;
@@ -481,6 +569,12 @@ class Item {
     }
   }
 
+  /**
+   * This function is called when the userclicks on the canvas and checks
+   * if the current item is being clicked on
+   *
+   * @return {boolean} true, if this item is being clicked on
+   */
   checkClick() {
     if (this.hover) {
       this.s.clickedItem = this;
