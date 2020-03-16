b9621a069f9991d9827423976ad1efdda9fc0c69
Alex <alex.baeuerle@uni-ulm.de>
2020-03-16 11:27:35 +0100

    Removed Unused Action Types
    There were some action types that were never used in the app. I now removed those.

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,11 +1,7 @@
 // types for the update functions in index.js and the relevant redux reducers
-export const UPDATE_PREDICTION = 'UPDATE_PREDICTION';
-export const UPDATE_LAYER_SIZE = 'UPDATE_LAYER_SIZE';
 export const UPDATE_TRAINING = 'UPDATE_TRAINING';
 export const UPDATE_NETWORK = 'UPDATE_NETWORK';
 export const UPDATE_APP_STATE = 'UPDATE_APP_STATE';
-export const UPDATE_LEARNING_RATE = 'UPDATE_LEARNING_RATE';
-export const UPDATE_ITERATION = 'UPDATE_ITERATION';
 export const TOGGLE_TRAINING = 'TOGGLE_TRAINING';
 export const STOP_TRAINING = 'STOP_TRAINING';
 export const UPDATE_UI = 'UPDATE_UI';
