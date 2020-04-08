2ef6d11ce9d6e1347fe8ef36823df6f25acba20a
Alex <alex.baeuerle@uni-ulm.de>
2020-04-08 10:59:38 +0200

    Output Styling Overview
    The output in the overview now got lines that animate to signal the data flow and have gotten a background color.

--- src/components/visualisation/sketches/model/overview/NetworkPlot.js
+++ src/components/visualisation/sketches/model/overview/NetworkPlot.js
@@ -47,6 +47,54 @@ export class NetworkPlot {
   }
 
   /**
+   * Plots an animation for incoming data.
+   *
+   * @param {String} type the type of data that we need to plot for
+   */
+  plotIncomingData(type) {
+    const s = this.s;
+    let height = this.halfH;
+    if (type === 'text') {
+      height = 2 * s.typography.fontsize;
+    }
+    const originX = s.outProps.left;
+    const originY = s.outProps.midY;
+    s.push();
+
+    // Draw background color
+    s.colors.lightgrey.setAlpha(100);
+    s.fill(s.colors.lightgrey);
+    s.colors.lightgrey.setAlpha(255);
+    for (let i = -1; i < 2; i=i+2) {
+      s.beginShape();
+      s.vertex(originX, originY);
+      s.bezierVertex(originX+80, originY, originX+20, originY+i*height,
+          originX+100, originY+i*height);
+      s.vertex(originX+100, originY);
+      s.endShape(s.CLOSE);
+    }
+    s.rectMode(s.CORNER);
+    s.rect(originX+100, originY-height, s.outProps.right-s.outProps.left-100,
+        2*height);
+
+    // Draw animated curves
+    s.noFill();
+    if (s.netAnim) {
+      s.stroke(s.colors.overview);
+      s.drawingContext.lineDashOffset = this.s.rev ?
+        s.frameCount/2 : -s.frameCount/2;
+      s.drawingContext.setLineDash([10, 10]);
+    } else {
+      s.stroke(s.colors.bluegrey);
+    }
+    for (let i = -1; i < 2; i=i+2) {
+      s.bezier(originX, originY, originX+80, originY, originX+20,
+          originY+i*height, originX+100, originY+i*height);
+    }
+    s.pop();
+  }
+
+  /**
    * Draws the text output of the network
    */
   plotText() {
@@ -84,11 +132,11 @@ export class NetworkPlot {
         left = 0;
       }
       s.noStroke();
-      s.colors.lightgrey.setAlpha(100);
+      s.colors.lightgrey.setAlpha(200);
       s.fill(s.colors.lightgrey);
       s.colors.lightgrey.setAlpha(255);
       s.rect(left + (right - left) / 2, 0, this.scale * (right-left),
-          3*s.typography.fontsize, 10);
+          4*s.typography.fontsize, 10);
     }
     s.strokeWeight(1 * this.scale);
 
@@ -162,6 +210,7 @@ export class NetworkPlot {
     const s = this.s;
     let yOff = 0;
     let showSteps = this.in + this.out;
+
     if (s.plotAnim && s.plotFrame < s.plotMoveFrames) {
       // calc the position of a moving plot
       yOff = -s.outProps.height/3 * (1 - (s.plotFrame / s.plotMoveFrames));
@@ -182,15 +231,16 @@ export class NetworkPlot {
       (s.height/4);
     s.push();
     s.translate(this.cx, this.cy + yOff);
+
     s.strokeWeight(2 * this.scale);
     s.colors.darkbluegrey.setAlpha(this.vis);
     s.stroke(s.colors.darkbluegrey);
     s.colors.darkbluegrey.setAlpha(255);
     s.line(-this.halfW * this.scale, 0, this.halfW * this.scale, 0);
     s.line(this.scale * ((-this.halfW) + (this.in * this.stepWidth)),
-        -this.halfH * this.scale,
+        -(this.halfH-20) * this.scale,
         this.scale * (-this.halfW + (this.in * this.stepWidth)),
-        this.scale * this.halfH
+        this.scale * (this.halfH-20)
     );
     // draw the scan box while animating
     if (s.plotAnim && s.plotFrame > s.plotMoveFrames &&
@@ -201,7 +251,7 @@ export class NetworkPlot {
         left = -this.halfW;
       }
       s.noStroke();
-      s.colors.lightgrey.setAlpha(100);
+      s.colors.lightgrey.setAlpha(200);
       s.fill(s.colors.lightgrey);
       s.colors.lightgrey.setAlpha(255);
       s.rect(left + (right - left) / 2, 0, this.scale * (right-left),
@@ -344,9 +394,15 @@ export class NetworkPlot {
       this.stepWidth = this.plotWidth / this.total;
     }
     if (this.s.props.training.inputType === 'Text Data') {
+      if (this.index === 2) {
+        this.plotIncomingData('text');
+      }
       this.plotText();
       this.plotHeading();
     } else {
+      if (this.index === 2) {
+        this.plotIncomingData('function');
+      }
       this.plotFunction();
       this.plotHeading();
     }
